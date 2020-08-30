3ceb711d645b71ce6108138caf280a600cdc7de4
unknown <prajapatiavtar@gmail.com>
2020-08-30 16:07:08 +0530

    add styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import Search from "./components/Search";
+import "./style/base.scss";
+import "./style/variable.scss";
+import "./style/include.scss";
 
 export class App extends Component {
   render() {
