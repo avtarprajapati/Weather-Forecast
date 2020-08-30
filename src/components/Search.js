555c57287eee71cca96614b990dce368ccdb9623
unknown <prajapatiavtar@gmail.com>
2020-08-30 15:20:25 +0530

    add component

---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Search() {
+  return (
+    <form>
+      <input type="text" placeholder="Search your favorite city" />
+    </form>
+  );
+}
+
+export default Search;
