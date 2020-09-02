194390e8151b45261dc6e1b0101fef5332402522
unknown <prajapatiavtar@gmail.com>
2020-09-02 13:04:19 +0530

    add first view

---
+++ src/components/FirstView.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class FirstView extends Component {
+  render() {
+    return (
+      <div className="show first-view">
+        <h2>To Search your favorite city know 5 days forecast.</h2>
+      </div>
+    );
+  }
+}
+
+export default FirstView;
